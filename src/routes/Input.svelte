2f43ca50984210278ee925ac76b84d39b4a97603
Dmitry Sorokin <soradimichi@gmail.com>
2025-06-06 12:16:41 +0600

    border coloring

--- src/routes/Input.svelte
+++ src/routes/Input.svelte
@@ -7,12 +7,13 @@
 		handleKeyNavigation: (index: number) => (event: KeyboardEvent) => void;
 		handleInput: (index: number) => (event: any) => void;
 	};
+	let isFocused = $state(false);
 	const { isComplete, isCorrect, index, handleKeyNavigation, handleInput, numbers }: ButtonProps =
 		$props();
 </script>
 
 <div
-	class={`group relative flex h-12 w-10 items-center justify-center overflow-hidden rounded-lg border px-3 text-2xl ${
+	class={`group relative flex h-12 w-10 items-center justify-center overflow-hidden rounded-lg border ${!numbers[index] ? 'border-[#798694]' : 'border-[#3C8AC6]'} px-3 text-2xl ${
 		isComplete && !isCorrect
 			? 'border-rose-500 bg-[#FEF7F6] text-rose-500'
 			: 'text-[#3C8AC6]  focus-within:border-[#3C8AC6]'
@@ -23,6 +24,8 @@
 		class="no-cursor h-full w-full translate-y-[0%] transform border-[#3c8ac6] text-transparent transition-transform duration-500 ease-out focus:translate-y-[-15%] focus:border-b focus:outline-none"
 		type="number"
 		maxlength="1"
+		onfocus={() => (isFocused = true)}
+		onblur={() => (isFocused = false)}
 		oninput={(event: any) => {
 			if (!event.target) return;
 			const value = event.target?.value.slice(0, 1);
